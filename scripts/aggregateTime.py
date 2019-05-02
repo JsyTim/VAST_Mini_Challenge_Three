0c0909b3ebaf16715e031aca44eca531af9ebe19
Artlands <artlands@users.noreply.github.com>
2019-05-01 23:05:42 -0500

    add heatmap data

--- scripts/aggregateTime.py
+++ scripts/aggregateTime.py
@@ -1,26 +1,39 @@
 # Read csv file, aggregate value in 1 hour
 import pandas as pd
 import numpy as np
+from functools import reduce
 
-is_check = {}
-dfList = {}
-outDf = pd.DataFrame()
+for f in range(1, 20):
+    timeStep = "2H"
+    is_check = {}
+    dfList = {}
+    maxList = {}
+    meanList = {}
+    outDf = []
 
-df = pd.read_csv("../data/SortByRegid/Region1-MobileSensor.csv")
+    infilename = "../data/SortByRegid/Region"+ str(f) + "-MobileSensor.csv"
+    ourfilename = "../data/HeatmapData/Region" + str(f) + ".csv"
+    df = pd.read_csv(infilename, parse_dates = True, index_col = "Timestamp")
 
-# print(df.head(4))
-# print(df.tail(4))
+    sensorIdSet = df['Sensor-id'].unique()
+    # print(sensorIdSet)
 
-sensorIdSet = df['Sensor-id'].unique()
-# print(sensorIdSet)
+    timeIndex = pd.date_range(start = '2020-04-06 00:00:00', end = '2020-04-10 23:00:00', freq = timeStep)
 
-for i, id in enumerate(sensorIdSet):
-    is_check[i] = df['Sensor-id'] == id
-    dfList[i] = df[is_check[i]]
-    print(dfList[i].shape)
+    for i, id in enumerate(sensorIdSet):
+        is_check[i] = df['Sensor-id'] == id
+        dfList[i] = df[is_check[i]]
 
-timeIndex = pd.date_range(start = ' 2020-04-06', end = ' 2020-04-10', freq = '1H')
-# print(timeIndex)
+        # meanList[i] = dfList[i]['Value'].resample(timeStep).mean().reindex(timeIndex)
+        maxList[i] = dfList[i]['Value'].resample(timeStep).max().reindex(timeIndex)
 
-# outDf['Value'] = dfList[0]['Value'].resample('1H').mean()
-# print(outDf.head(4))
+        maxDf = pd.DataFrame({'Timestamp': maxList[i].index, sensorIdSet[i]: maxList[i].values, str(sensorIdSet[i]) + "flag": maxList[i].values})
+
+        maxDf[str(sensorIdSet[i]) + "flag"] = (maxDf[str(sensorIdSet[i]) + "flag"] > 150 ).astype(int)
+        outDf.append(maxDf)
+        # print(outDf[i].head(100))
+
+    df_final = reduce( lambda left,right: pd.merge(left, right, on='Timestamp'), outDf)
+
+    # print(df_final.shape)
+    df_final.to_csv(ourfilename, index=False)
